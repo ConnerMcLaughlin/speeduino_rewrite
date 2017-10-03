70cd7632604fb2c56d4054e5cb3a671fad0cd061
ConnerMcLaughlin <conner.mclaughlin@yahoo.com>
2017-10-02 21:16:06 -0500

    Initial Fork
    Left out all of the hardware docs

---
+++ speeduino/corrections.h
@@ -0,0 +1,34 @@
+/*
+All functions in the gamma file return
+
+*/
+#ifndef CORRECTIONS_H
+#define CORRECTIONS_H
+
+void initialiseCorrections();
+
+byte correctionsFuel();
+static inline byte correctionWUE(); //Warmup enrichment
+static inline byte correctionCranking(); //Cranking enrichment
+static inline byte correctionASE(); //After Start Enrichment
+static inline byte correctionAccel(); //Acceleration Enrichment
+static inline byte correctionFloodClear(); //Check for flood clear on cranking
+static inline byte correctionAFRClosedLoop(); //Closed loop AFR adjustment
+static inline byte correctionFlex(); //Flex fuel adjustment
+static inline byte correctionBatVoltage(); //Battery voltage correction
+static inline byte correctionIATDensity(); //Inlet temp density correction
+static inline byte correctionLaunch(); //Launch control correction
+static inline bool correctionDFCO(); //Decelleration fuel cutoff
+
+int8_t correctionsIgn(int8_t advance);
+static inline int8_t correctionFixedTiming(int8_t);
+static inline int8_t correctionCrankingFixedTiming(int8_t);
+static inline int8_t correctionFlexTiming(int8_t);
+static inline int8_t correctionIATretard(int8_t);
+static inline int8_t correctionSoftRevLimit(int8_t);
+static inline int8_t correctionSoftLaunch(int8_t);
+static inline int8_t correctionSoftFlatShift(int8_t);
+
+uint16_t correctionsDwell(uint16_t dwell);
+
+#endif // CORRECTIONS_H
